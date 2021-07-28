b4f25e100976e633b81c31b41f1bab9d6494d7d6
Noelia-Elizabeth <elizabeth.nmacleod@gmail.com>
2021-07-28 12:24:38 -0300

    Initial commit

---
+++ Ejercicio1/src/ejercicio1/Ejercicio1.java
@@ -0,0 +1,39 @@
+
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+        
+        Scanner scanner = new Scanner(System.in);
+        Libro libro1 = new Libro();
+        
+        cargarLibro(libro1, scanner);
+        
+        mostrarLibro (libro1);
+        
+        
+    }
+    
+    public static void cargarLibro (Libro libro1, Scanner scanner) {
+        System.out.println("Ingrese el ISBN del libro:");
+        libro1.isbn = scanner.next();
+        System.out.println("Ingrese el título del libro:");
+        libro1.titulo = scanner.next();
+        System.out.println("Ingrese el autor del libro:");
+        libro1.autor = scanner.next();
+        System.out.println("Ingrese el número de páginas del libro:");
+        libro1.cantidadPaginas = scanner.nextInt();        
+        
+    }
+    
+    public static void mostrarLibro(Libro libro1) {
+        System.out.println("Libro: " + libro1.titulo);
+        System.out.println("Autor: " + libro1.autor);
+        System.out.println("ISBN: " + libro1.isbn);
+        System.out.println("Cantidad de páginas: " + libro1.cantidadPaginas);
+    }
+    
+}
---
+++ Ejercicio1/src/ejercicio1/Libro.java
@@ -0,0 +1,23 @@
+
+package ejercicio1;
+
+public class Libro {
+    
+    public String isbn;
+    public String titulo;
+    public String autor;
+    public int cantidadPaginas;
+
+    public Libro() {
+    }
+    
+    public Libro(String isbn, String titulo, String autor, int cantidadPaginas) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+        this.cantidadPaginas = cantidadPaginas;
+    }
+    
+    
+    
+}
