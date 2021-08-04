15abee9df4e1a0fc54f8806e27912c1036001d57
Noelia-Elizabeth <elizabeth.nmacleod@gmail.com>
2021-08-04 12:54:45 -0300

    [ADD] Agrego extra 4 incompleto

---
+++ Extra4/src/extra4/NIF.java
@@ -0,0 +1,27 @@
+
+package extra4;
+
+public class NIF {
+    
+    private int dni;
+    private String letra;
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getLetra() {
+        return letra;
+    }
+
+    public void setLetra(String letra) {
+        this.letra = letra;
+    }
+    
+    
+    
+}
